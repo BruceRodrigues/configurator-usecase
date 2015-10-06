b858c4860ce21574e49003fa52c00a1aa97a99f5
Bruce Rodrigues <brucerodrigues@github.com>
2015-10-05 23:08:59 -0300

    estrutura para caso de uso web

---
+++ web/src/main/webapp/VAADIN/widgetsets/br.ufsc.usecase.MyAppWidgetset/deferredjs/05C083F75743F6384A4FA0FBB3A5FCD0/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.br_ufsc_usecase_MyAppWidgetset.runAsyncCallback2('Bbb(1540,1,cTd);_.tc=function Wac(){gZb((!_Yb&&(_Yb=new lZb),_Yb),this.a.d)};GMd(Th)(2);\n//# sourceURL=br.ufsc.usecase.MyAppWidgetset-2.js\n')
